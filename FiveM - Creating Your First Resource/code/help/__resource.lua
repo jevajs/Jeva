287590eda6c90e58ccb48b28c59618b57a3ab458
ThatZiv <thatziv@gmail.com>
2018-11-13 16:58:01 -0800

    fivem scripting (new)

---
+++ FiveM - Creating Your First Resource/code/help/__resource.lua
@@ -0,0 +1,7 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+--[[
+    Called 'help-c.lua' due to 'c' standing for client and 'help' part as the resource
+    This is good for debugging resources
+--]]
+client_script 'help-c.lua' 
---
+++ FiveM - Creating Your First Resource/code/help/help-c.lua
@@ -0,0 +1,16 @@
+--[[ help script made at JEVA by Zua (https://github.com/thatziv) ]]
+
+
+-- This function is used to register a command for the chat. 
+--When this is exectuted, it will execute a function, doing the msg function to the client
+RegisterCommand('help', function()
+    msg("Discord: discord.gg/xyz")
+    msg("Website: website.com")
+    msg("teamspeak: website.com")
+end, false)
+
+-- We declare this 'msg' function on the bottom due to better practices.
+function msg(text)
+    -- TriggerEvent will send a chat message to the client in the prefix as red
+    TriggerEvent("chatMessage",  "[Server]", {255,0,0}, text)
+end
