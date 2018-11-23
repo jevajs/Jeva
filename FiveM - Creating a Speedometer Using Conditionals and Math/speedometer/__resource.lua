df77df10f47afdf80e5e1b44dfe72357b3592df8
ThatZiv <thatziv@gmail.com>
2018-11-22 21:21:23 -0800

    speedometer

---
+++ FiveM - Creating a Speedometer Using Conditionals and Math/speedometer/__resource.lua
@@ -0,0 +1,1 @@
+client_script "speedometer-c.lua"
---
+++ FiveM - Creating a Speedometer Using Conditionals and Math/speedometer/speedometer-c.lua
@@ -0,0 +1,24 @@
+local mph = true 
+
+local mph = 2.2369
+local kph = 3.6
+
+function text(content) 
+    SetTextFont(1)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.9,0.5)
+end
+Citizen.CreateThread(function()
+
+    while true do
+        Citizen.Wait(1)
+        local speed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*mph)
+        -- Condition to check if the ped is in a vehicle
+        if(IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+            text(math.floor(speed))
+        end
+    end
+end)
