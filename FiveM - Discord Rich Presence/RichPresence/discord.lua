423167ed08be3ab837a2560b85dc24b010c89ffc
ThatZiv <thatziv@gmail.com>
2019-06-13 20:46:25 -0400

    Discord Rich presense

---
+++ FiveM - Discord Rich Presence/RichPresence/discord.lua
@@ -0,0 +1,25 @@
+Citizen.CreateThread(function()
+    while true do
+        local player = GetPlayerPed(-1)
+        
+        Citizen.Wait(5*1000) -- checks every 5 seconds (to limit resource usage)
+        
+        SetDiscordAppId(588877885281140753) -- client id (int)
+
+        SetRichPresence( GetPlayerName(source) .. " is on " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))) )) -- main text (string)
+
+        SetDiscordRichPresenceAsset("big") -- large logo key (string)
+        SetDiscordRichPresenceAssetText(GetPlayerName(source)) -- Large logo "hover" text (string)
+
+        SetDiscordRichPresenceAssetSmall("zua") -- small logo key (string)
+        SetDiscordRichPresenceAssetSmallText("Health: "..(GetEntityHealth(player)-100)) -- small logo "hover" text (string)
+
+    end
+end)
+
+--[[
+    EVAL STRING FOR VIDEO 
+
+    /eval SetEntityHealth(GetPlayerPed(-1),100)
+    
+--]]
