fb9c55a81355b869a0471eb426641e5004b32f8f
ThatZiv <thatziv@gmail.com>
2019-07-28 16:47:13 -0400

    rename

--- FiveM - Intro to JS Scripting/jsChat/src/server.js
+++ FiveM - Intro to JS Scripting/jsChat/src/server.js
@@ -1,7 +1,7 @@
 // this event will listen through both scopes for a trigger
 onNet("js:chat", (msg, color) => {
     emitNet("chat:addMessage", -1, {
-        args: [msg], 
+        args: [msg],
         color: color
     })
     console.log(msg)
