06974dfff60e124e049aa70c850de494a5f5cf98
Zavaar Shah <35277113+ThatZiv@users.noreply.github.com>
2019-07-29 12:56:45 -0400

    better practices

--- FiveM - Intro to JS Scripting/jsChat/src/client.js
+++ FiveM - Intro to JS Scripting/jsChat/src/client.js
@@ -5,8 +5,7 @@
 // chat command using net events (server/client)
 RegisterCommand("jschat", async (source, args) => {
     let argString = args.join(" ")
-    if (!argString) {argString = "Nothing..."}
-    emitNet("js:chat", argString, [0,255,0]) // this will trigger it
+    emitNet("js:chat", (argString ? argString : "Nothing.."), [0,255,0]) // this will trigger it
     return
 })
 
